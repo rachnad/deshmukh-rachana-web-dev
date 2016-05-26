4be76b6ba951df1bcdc5ef9a9d7edfa018e86bbf
Rachana <deshmukh.r@husky.neu.edu>
2016-05-26 18:57:40 -0400

    Finished pages service api

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -13,15 +13,57 @@
             { "_id": "543", "name": "Post 3", "websiteId": "456" }
         ];
         var api = {
-            "createPage"   : "createPage",
-            "findPageByWebsiteId" : "findPageByWebsiteId",
-            "findPageById" : "findPageById",
-            "updatePage" : "updatePage",
-            "deletePage" : "deletePage"
+            createPage   : createPage,
+            findPageByWebsiteId : findPageByWebsiteId,
+            findPageById : findPageById,
+            updatePage : updatePage,
+            deletePage : deletePage
         };
         return api;
-        function createUser(user) { }
-        function findUserById(id) { }
 
+
+        function createPage(page, websiteID) {
+            page.websiteId = websiteID;
+            pages.push(page);
+        }
+
+        function findPageByWebsiteId(websiteID) {
+            for (var i in pages) {
+                if (pages[i].websiteId === websiteID) {
+                    return pages[i];
+                }
+            }
+            return null;
+        }
+
+        function findPageById(pageID) {
+            for (var i in pages) {
+                if (pages[i]._id === pageID) {
+                    return pages[i];
+                }
+            }
+            return null;
+        }
+
+
+        function updatePage(pageID, newPage) {
+            for (var i in users)  {
+                if (pages[i]._id === userID) {
+                    pages[i].name = newPage.name;
+                    pages[i].websiteId = newPage.websiteId;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function deletePage(pageID) {
+            for (var i in pages) {
+                if (pages[i]._id === pageID) {
+                    pages.splice(pages[i], 1)
+                }
+            }
+            return null;
+        }
     }
 })();

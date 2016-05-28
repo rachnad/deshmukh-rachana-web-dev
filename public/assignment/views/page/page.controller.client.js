dc885148d275599558758b20cb5bd54dd350ad54
Rachana <deshmukh.r@husky.neu.edu>
2016-05-28 15:09:43 -0400

    Completed page functionality

--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -7,6 +7,77 @@
         .module("WebAppMaker")
         .controller("PageListController", PageListController)
         .controller("NewPageController", NewPageController)
-        .controller("EditPageController", EditPageController)
+        .controller("EditPageController", EditPageController);
+
+
+    function PageListController($routeParams, PageService){
+        var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+
+        function init() {
+            vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
+            console.log(vm.pages);
+        }
+        init();
+    }
+
+    function NewPageController($routeParams, PageService) {
+        var vm = this;
+        vm.websiteId = $routeParams.wid;
+        vm.userId = $routeParams.uid;
+
+        vm.createPage = createPage;
+        vm.added = false;
+
+        function createPage() {
+            var newPage = {"_id": (new Date).getTime().toString(),
+                "name": vm.page.name,
+                "websiteId": vm.websiteId,
+                "title": vm.page.title
+            };
+
+            console.log(newPage);
+
+            if (!vm.added) {
+                PageService.createPage(newPage, vm.websiteId);
+                vm.added = true;
+            }
+        }
+
+
+    }
+
+    function EditPageController($routeParams, $location, PageService) {
+        var vm = this
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.updatePage = updatePage;
+        vm.deletePage = deletePage;
+
+        function init() {
+            vm.page = angular.copy(PageService.findPageById(vm.pageId));
+        }
+        init();
+
+
+        function updatePage() {
+            var result = PageService.updatePage(vm.pageId, vm.page);
+            if(result === true) {
+                vm.success = "Website successfully updated";
+            } else {
+                vm.error = "Website not updated";
+            }
+
+        }
+
+        function deletePage() {
+            PageService.deletePage(vm.pageId);
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+        }
+
+
+    }
 
 })();

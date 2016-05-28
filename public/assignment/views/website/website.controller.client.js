d7216d2641d65ef98bbe1776e820394eea8a78f4
Rachana <deshmukh.r@husky.neu.edu>
2016-05-28 15:09:53 -0400

    Completed website functionality

--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -6,6 +6,71 @@
         .module("WebAppMaker")
         .controller("WebsiteListController", WebsiteListController)
         .controller("NewWebsiteController", NewWebsiteController)
-        .controller("EditWebsiteController", EditWebsiteController)
+        .controller("EditWebsiteController", EditWebsiteController);
+
+
+
+    function WebsiteListController($routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = $routeParams["uid"];
+
+        function init() {
+            vm.websites = WebsiteService.findWebsiteByUser(vm.userId);
+        }
+        init();
+    }
+
+
+    function NewWebsiteController($routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.createWebsite = createWebsite;
+        vm.added = false;
+
+        function createWebsite() {
+            var website = {"_id": (new Date).getTime().toString(),
+                            "name": vm.website.name,
+                            "developerId": vm.userId,
+                            "description": vm.website.description};
+
+            if (!vm.added) {
+                var result = WebsiteService.createWebsite(website);
+                vm.added = true;
+            }
+        }
+
+    }
+
+    function EditWebsiteController($location, $routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.updateWebsite = updateWebsite;
+        vm.deleteWebsite = deleteWebsite;
+
+        function init() {
+            vm.website = angular.copy(WebsiteService.findWebsiteById(vm.websiteId));
+        }
+        init();
+
+
+        function updateWebsite() {
+            var result = WebsiteService.updateWebsite(vm.websiteId, vm.website);
+            if(result === true) {
+                vm.success = "Website successfully updated";
+            } else {
+                vm.error = "Website not updated";
+            }
+
+        }
+
+        function deleteWebsite() {
+            WebsiteService.deleteWebsite(vm.websiteId);
+            $location.url("/user/" + vm.userId + "/website");
+
+        }
+
+    }
+
 
 })();

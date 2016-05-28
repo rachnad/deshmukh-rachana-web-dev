73b9511cea5b2cb0b0155249860bea2186b12985
Rachana <deshmukh.r@husky.neu.edu>
2016-05-27 20:50:39 -0400

    Completed user login, registration and profile

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -5,7 +5,8 @@
     angular
         .module("WebAppMaker")
         .controller("LoginController", LoginController)
-        .controller("RegisterController", RegisterController);
+        .controller("RegisterController", RegisterController)
+        .controller("ProfileController", ProfileController);
 
         function LoginController($location, UserService) {
             var vm = this;
@@ -15,7 +16,7 @@
                 var user = UserService.findUserByCredentials(username, password);
                 if (user) {
                     var id = user._id;
-                    $location.url("/user" + id);
+                    $location.url("/user/" + id);
                 }
                 else {
                     vm.error = "User not found";
@@ -23,6 +24,42 @@
             }
         }
 
-        function RegisterController() {}
+        function RegisterController($location, UserService) {
+            var vm = this;
+            vm.addUser = addUser;
+
+            function addUser() {
+                var newUser = {"_id": (new Date).getTime().toString(),
+                                "username": vm.user.username,
+                                "password": vm.user.password
+                };
+
+                UserService.createUser(newUser);
+                $location.url("/user/" + newUser._id);
+            }
+        }
+
+        function ProfileController($routeParams, UserService) {
+            var vm = this;
+            vm.updateUser = updateUser;
+            //get userID from url
+            vm.userId = $routeParams["uid"];
+
+
+            function init() {
+                vm.user = angular.copy(UserService.findUserById(vm.userId));
+                console.log(vm.user);
+            }
+            init();
+
+            function updateUser() {
+                var result = UserService.updateUser(vm.userId, vm.user);
+                if(result === true) {
+                    vm.success = "User successfully updated";
+                } else {
+                    vm.error = "User not found";
+                }
+            }
+        }
 
 })();

17aff6ff0f92901f764eb33151085612dfc38db4
Rachana <deshmukh.r@husky.neu.edu>
2016-06-02 16:32:56 -0400

    Use flickr api to search and select images for widget

---
+++ public/assignment/views/widget/widget-flickr-search.controller.client.js
@@ -0,0 +1,52 @@
+/**
+ * Created by rachanadeshmukh on 6/1/16.
+ */
+(function() {
+    angular
+        .module("WebAppMaker")
+        .controller("FlickrImageSearchController", FlickrImageSearchController);
+
+    function FlickrImageSearchController($routeParams, FlickrService, WidgetService, $location) {
+        vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
+        vm.searchPhotos = searchPhotos;
+        vm.selectPhoto = selectPhoto;
+
+        function init() {
+            WidgetService
+                .findWidgetById(vm.widgetId)
+                .then(function(response) {
+                    vm.widget = response.data;
+                })
+        }
+        init();
+
+        function searchPhotos(searchTerm) {
+            FlickrService
+                .searchPhotos(searchTerm)
+                .then(function(response) {
+                    var data = response.data.replace("jsonFlickrApi(","");
+                    data = data.substring(0,data.length - 1);
+                    data = JSON.parse(data);
+                    vm.photos = data.photos;
+                });
+        }
+
+
+        function selectPhoto(photo) {
+            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
+            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
+            vm.widget.url = url;
+            vm.widget.width = 100;
+            WidgetService
+                .updateWidget(vm.widgetId, vm.widget)
+                .then(function (response) {
+                    $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page/"+vm.pageId+"/widget/"+vm.widgetId);
+                })
+        }
+
+    }
+})();

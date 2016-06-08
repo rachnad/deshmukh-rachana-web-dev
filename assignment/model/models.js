ae1685bef32d2fbaf02be4b1f3b0845aa0c9aefc
Rachana <deshmukh.r@husky.neu.edu>
2016-06-08 13:54:52 -0400

    Openshift issue

--- assignment/model/models.js
+++ assignment/model/models.js
@@ -1,33 +1,4 @@
 module.exports = function() {
-
-    var mongoose = require('mongoose');
-    var connectionString = 'mongodb://127.0.0.1:27017/web-dev';
-
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }
-
-    var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
-
-   // Connect to mongodb
-    var connect = function () {
-        mongoose.connect(url);
-    };
-    connect();
-
-    var db = mongoose.connection;
-
-    db.on('error', function(error){
-        console.log("Error loading the db - "+ error);
-    });
-
-    db.on('disconnected', connect);
-
     //mongoose.connect('mongodb://localhost/web-dev');
 
     var userModel = require("./user/user.model.server.js")();
--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://127.0.0.1:27017/web-dev';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect(connectionString);
+
+// Connect to mongodb
+var connect = function () {
+    mongoose.connect(url);
+};
+connect();
+
+var db = mongoose.connection;
+
+db.on('error', function(error){
+    console.log("Error loading the db - "+ error);
+});
+
+db.on('disconnected', connect);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -8,11 +34,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-var meanTest = require ("./test/app.js");
+//var meanTest = require ("./test/app.js");
 var assignment = require("./assignment/app.js");
+var project = require("./project/app.js");
 
-meanTest(app);
+//meanTest(app);
 assignment(app);
+project(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;

cf2de62850771c96ce0df72ae471eddfe6285911
Rachana <deshmukh.r@husky.neu.edu>
2016-06-18 11:05:17 -0400

    Mobile

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://webdev-deshmukhrachana.rhcloud.com");
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });

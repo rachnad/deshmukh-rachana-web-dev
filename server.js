b32fa5cf06731ef8d67225970a851b3a1243b8e6
Rachana <deshmukh.r@husky.neu.edu>
2017-01-18 12:16:25 -0500

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://webdev-deshmukhrachana.rhcloud.com");
+    res.header("Access-Control-Allow-Origin", "http://personal-deshmukhrachana.rhcloud.com");
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
